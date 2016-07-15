b7ef70392c502b00e778a2c5f9a7bdaaeb3eb747
Felipe <felipesantos2089@gmail.com>
2016-07-15 10:41:11 -0300

    alteração de pastas do recadastramento e negativação para o nfs

--- comercial/scripts/20160714200943_alteracao_pastas_recadastramento_cobranca.sql
+++ comercial/scripts/20160714200943_alteracao_pastas_recadastramento_cobranca.sql
@@ -1,9 +1,21 @@
 -- // alteracao_pastas_recadastramento_cobranca
 -- Migration SQL that makes the change goes here.
 
+update atualizacaocadastral.imagem_retorno set imgr_pathimagem =  'recadastramento' || imgr_pathimagem;
 
+update atualizacaocadastral.imagem_retorno
+set imgr_pathimagem = substring(imgr_pathimagem from 0 for position('images' in imgr_pathimagem) - 0) 
+ || 'imagem_retorno'
+ || substring(imgr_pathimagem from position('images' in imgr_pathimagem) + 6);
+
+ update cadastro.imovel_imagem
+set imim_caminhoimagem =  'recadastramento' || imim_caminhoimagem;
 
 -- //@UNDO
 -- SQL to undo the change goes here.
 
+update atualizacaocadastral.imagem_retorno
+set imgr_pathimagem = '/images' || substring(imgr_pathimagem from position('imagem_retorno' in imgr_pathimagem) + 14);
 
+update cadastro.imovel_imagem
+set imim_caminhoimagem =  substring(imim_caminhoimagem from position('/imovel_imagem' in imim_caminhoimagem));
