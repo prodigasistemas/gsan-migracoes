9e20ada599b27e27af5b2fe939dc28508645b386
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2016-07-21 18:03:01 -0300

    Associacao de quantidade de ocupantes tambem com atualizacao cadastral de imovel

---
+++ comercial/scripts/20160721132909_criar_tabela_para_quantidades_de_tipos_de_ocupantes_de_imovel_para_atualizacao_cadastral.sql
@@ -0,0 +1,50 @@
+-- // criar tabela para quantidades de tipos de ocupantes de imovel para atualizacao cadastral
+-- Migration SQL that makes the change goes here.
+
+CREATE TABLE cadastro.imovel_tipo_ocupante_quantidade_atualizacao_cadastral
+(
+  itqa_id                integer NOT NULL,
+  itqa_quantidade        integer NOT NULL,
+  itpo_id                integer NOT NULL,
+  imov_id                integer NOT NULL,
+  itqa_tmultimaalteracao timestamp without time zone NOT NULL DEFAULT now(),
+
+  CONSTRAINT imovel_tipo_ocupante_quantidade_atualizacao_cadastral_pkey PRIMARY KEY (itqa_id),
+  CONSTRAINT fk1_imovel_tipo_ocupante_quantidade_atualizacao_cadastral FOREIGN KEY (imov_id)
+      REFERENCES cadastro.imovel (imov_id) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT fk2_imovel_tipo_ocupante_quantidade_atualizacao_cadastral FOREIGN KEY (itpo_id)
+      REFERENCES cadastro.imovel_tipo_ocupante (itpo_id) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT
+)
+WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE                                   cadastro.imovel_tipo_ocupante_quantidade_atualizacao_cadastral OWNER TO gsan_admin;
+GRANT ALL ON TABLE                            cadastro.imovel_tipo_ocupante_quantidade_atualizacao_cadastral TO gsan_admin;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE cadastro.imovel_tipo_ocupante_quantidade_atualizacao_cadastral TO pg_aplic;
+GRANT SELECT ON TABLE                         cadastro.imovel_tipo_ocupante_quantidade_atualizacao_cadastral TO pg_users;
+
+COMMENT ON COLUMN cadastro.imovel_tipo_ocupante_quantidade_atualizacao_cadastral.itqa_id                 IS 'Identificacao';
+COMMENT ON COLUMN cadastro.imovel_tipo_ocupante_quantidade_atualizacao_cadastral.itqa_quantidade         IS 'Quantidade de ocupantes';
+COMMENT ON COLUMN cadastro.imovel_tipo_ocupante_quantidade_atualizacao_cadastral.itpo_id                 IS 'Identificacao do tipo de ocupante';
+COMMENT ON COLUMN cadastro.imovel_tipo_ocupante_quantidade_atualizacao_cadastral.imov_id                 IS 'Identificacao do imovel';
+COMMENT ON COLUMN cadastro.imovel_tipo_ocupante_quantidade_atualizacao_cadastral.itqa_tmultimaalteracao  IS 'Data e hora da ultima alteracao';
+
+CREATE SEQUENCE cadastro.seq_imovel_tipo_ocupante_quantidade_atualizacao_cadastral
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE                   cadastro.seq_imovel_tipo_ocupante_quantidade_atualizacao_cadastral OWNER TO gsan_admin;
+GRANT ALL ON TABLE            cadastro.seq_imovel_tipo_ocupante_quantidade_atualizacao_cadastral TO gsan_admin;
+GRANT SELECT, UPDATE ON TABLE cadastro.seq_imovel_tipo_ocupante_quantidade_atualizacao_cadastral TO pg_aplic;
+GRANT SELECT ON TABLE         cadastro.seq_imovel_tipo_ocupante_quantidade_atualizacao_cadastral TO pg_users;
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+DROP TABLE    cadastro.imovel_tipo_ocupante_quantidade_atualizacao_cadastral;
+DROP SEQUENCE cadastro.seq_imovel_tipo_ocupante_quantidade_atualizacao_cadastral;
