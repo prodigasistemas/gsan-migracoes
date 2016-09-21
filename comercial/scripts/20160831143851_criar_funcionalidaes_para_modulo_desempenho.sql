a917fead6945a72ed2669539c645dc64d3f08cee
Paulo Moura <paulociecomp@gmail.com>
2016-09-21 11:25:23 -0300

    correcao em migracoes do menu do seguranca.

--- comercial/scripts/20160831143851_criar_funcionalidaes_para_modulo_desempenho.sql
+++ comercial/scripts/20160831143851_criar_funcionalidaes_para_modulo_desempenho.sql
@@ -14,10 +14,10 @@ INSERT INTO seguranca.funcionalidade
 VALUES (16035, 13, 'Manter Acao', '2016-08-31 00:00:00', '', NULL, 2, 'MantAcao', 0, 2, 2, 147, 'http://hdesempenho.cosanpa.pa.gov.br/acoes/');
 
 INSERT INTO seguranca.funcionalidade 
-VALUES (16036, 13, 'Arquivo de Retorno', '2016-08-31 00:00:00', '', NULL, 2, 'ArqRet', 0, 2, 2, NULL, 'http://hdesempenho.cosanpa.pa.gov.br/imovel_retorno');
+VALUES (16036, 13, 'Arquivo de Retorno', '2016-08-31 00:00:00', '', NULL, 1, 'ArqRet', 0, 2, 2, 145, 'http://hdesempenho.cosanpa.pa.gov.br/imovel_retorno');
 
 INSERT INTO seguranca.funcionalidade 
-VALUES (16037, 13, 'Arquivo de Hidrometros', '2016-08-31 00:00:00', '', NULL, 2, 'ArqHid', 0, 2, 2, NULL, 'http://hdesempenho.cosanpa.pa.gov.br/hidrometro_instalacao_historicos/new');
+VALUES (16037, 13, 'Arquivo de Hidrometros', '2016-08-31 00:00:00', '', NULL, 1, 'ArqHid', 0, 2, 2, 145, 'http://hdesempenho.cosanpa.pa.gov.br/hidrometro_instalacao_historicos/new');
 
 -- //@UNDO
 -- SQL to undo the change goes here.
