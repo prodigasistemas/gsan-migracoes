8ec54ccc9f393c672c7237cc9eb0b3f2a80e6ca9
Pamela Gatinho <pamelagatinho@gmail.com>
2017-07-05 10:34:35 -0300

    Negociacoes - cobranca por resultado

--- comercial/scripts/20170612181124_Inserir_funcionalidade_de_negociacoes_ao_processo_de_pagamentos_COBRANCA_POR_RESULTADO.sql
+++ comercial/scripts/20170612181124_Inserir_funcionalidade_de_negociacoes_ao_processo_de_pagamentos_COBRANCA_POR_RESULTADO.sql
@@ -1,8 +1,8 @@
 -- // Inserir funcionalidade de negociacoes ao processo de pagamentos COBRANCA POR RESULTADO
 -- Migration SQL that makes the change goes here.
 
-insert into batch.processo_funcionalidade values (nextval('batch.seq_processo_funcionalidade'), 151, now(), 19, 3, 
-(select fncd_id from seguranca.funcionalidade where fncd_dsfuncionalidade like 'Gerar Movimento Negociacoes Contas Cobranca Empresa'  ), 1, null)
+--insert into batch.processo_funcionalidade values (nextval('batch.seq_processo_funcionalidade'), 151, now(), 19, 3, 
+--(select fncd_id from seguranca.funcionalidade where fncd_dsfuncionalidade like 'Gerar Movimento Negociacoes Contas Cobranca Empresa'  ), 1, null)
 
 
 -- //@UNDO
