88fc4d80438d389973dca91b09a15a7b37234195
Felipe <felipesantos2089@gmail.com>
2017-08-11 12:09:19 -0300

    Adicinar id do pagamento em EmpresaCobrancaContaPagamentos

---
+++ comercial/scripts/20170810131702_add_pgmt_id_em_empreca_cobranca_conta_pagamentos.sql
@@ -0,0 +1,9 @@
+-- // add pgmt_id em empreca cobranca conta pagamentos
+-- Migration SQL that makes the change goes here.
+
+ALTER TABLE cobranca.empr_cobr_conta_pagto ADD COLUMN pgmt_id integer NOT NULL;
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+ALTER TABLE cobranca.empr_cobr_conta_pagto DROP COLUMN pgmt_id;
