dadf1f03fcd077aea24a58139a85f4bb2cb1dc34
Felipe <felipesantos2089@gmail.com>
2017-09-01 11:40:30 -0300

    Cancelamento de Parcelamento

--- comercial/scripts/20170622186027_Criar_data_para_inicio_de_cancelamento_de_parcelamentos.sql
+++
@@ -1,9 +0,0 @@
--- // Criar data para inicio de cancelamento de parcelamentos
--- Migration SQL that makes the change goes here.
-INSERT INTO cobranca.parametro(id, nome, valor) VALUES (nextval('cobranca.seq_cobranca_parametro'), 'DATA_INICIO_PARCELAMENTO_GSAN', '01/01/2010');
-
-
--- //@UNDO
--- SQL to undo the change goes here.
-DELETE FROM cobranca.parametro WHERE id = 1;
-
--- comercial/scripts/20170810131702_add_pgmt_id_em_empreca_cobranca_conta_pagamentos.sql
+++ comercial/scripts/20170810131702_add_pgmt_id_em_empreca_cobranca_conta_pagamentos.sql
@@ -1,7 +1,7 @@
 -- // add pgmt_id em empreca cobranca conta pagamentos
 -- Migration SQL that makes the change goes here.
 
-ALTER TABLE cobranca.empr_cobr_conta_pagto ADD COLUMN pgmt_id integer NOT NULL;
+ALTER TABLE cobranca.empr_cobr_conta_pagto ADD COLUMN pgmt_id integer;
 
 -- //@UNDO
 -- SQL to undo the change goes here.
