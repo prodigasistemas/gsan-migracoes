91129b8874b34890cf7009fa9c0a0e67f275de8e
Matheus Cruz <matheus1souza27@gmail.com>
2017-11-09 16:34:56 -0300

    Adicionando indicador de exclusao e datas de registro e exclusao de boletos do BB

--- comercial/scripts/20171026212243_adicionar_indicadores_tabela_boleto_info.sql
+++ comercial/scripts/20171026212243_adicionar_indicadores_tabela_boleto_info.sql
@@ -2,9 +2,14 @@
 -- Migration SQL that makes the change goes here.
 ALTER TABLE arrecadacao.boleto_info ADD COLUMN boin_icgeradogsan smallint;
 ALTER TABLE arrecadacao.boleto_info ADD COLUMN boin_icregistradobanco smallint;
-
+ALTER TABLE arrecadacao.boleto_info ADD COLUMN boin_dtregistrobanco timestamp;
+ALTER TABLE arrecadacao.boleto_info ADD COLUMN boin_icexclusaobanco smallint;
+ALTER TABLE arrecadacao.boleto_info ADD COLUMN boin_dtexclusaobanco timestamp;
 
 -- //@UNDO
 -- SQL to undo the change goes here.
 ALTER TABLE arrecadacao.boleto_info DROP COLUMN boin_icgeradogsan;
 ALTER TABLE arrecadacao.boleto_info DROP COLUMN boin_icregistradobanco;
+ALTER TABLE arrecadacao.boleto_info DROP COLUMN boin_dtregistrobanco;
+ALTER TABLE arrecadacao.boleto_info DROP COLUMN boin_icexclusaobanco;
+ALTER TABLE arrecadacao.boleto_info DROP COLUMN boin_dtexclusaobanco;
