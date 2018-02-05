2add2a3f6b5d209f46ad302adc0999b658159502
Matheus Cruz <matheus1souza27@gmail.com>
2018-02-05 15:49:48 -0300

    Adicionando funcionalidade de desfazer parcelamentos no batch 49 - Classificar pagamentos

--- comercial/scripts/20180201192322_adicionar_funcionalidade_desfazer_parcelamento_no_batch_classificar_pagamento.sql
+++ comercial/scripts/20180201192322_adicionar_funcionalidade_desfazer_parcelamento_no_batch_classificar_pagamento.sql
@@ -1,10 +1,12 @@
--- // migrar funcionalidade desfazer parcelamento para batch classificar pagamento
+-- // adicionar funcionalidade desfazer parcelamento no batch classificar pagamento
 -- Migration SQL that makes the change goes here.
-INSERT INTO batch.processo_funcionalidade VALUES (nextval('batch.seq_processo_funcionalidade'), 49, now(), 5, 0, 652, 1, null);
+INSERT INTO seguranca.funcionalidade VALUES (16044, 3, 'Desfazer Parcel por Entrada nao Paga sem ano mes faturamento', now(), 'Gsan/', 'sem url', 2, 'DEPAENSAMF',1,2,2,null,null);
+INSERT INTO batch.processo_funcionalidade VALUES (nextval('batch.seq_processo_funcionalidade'), 49, now(), 5, 3, 16044, 1, null);
 INSERT INTO cobranca.parametro VALUES (2, 'QUANTIDADE_DIAS_VENCIMENTO_GUIA', '7');
 
 
 -- //@UNDO
 -- SQL to undo the change goes here.
-DELETE FROM batch.processo_funcionalidade WHERE proc_id = 49 and fncd_id = 652;
+DELETE FROM batch.processo_funcionalidade WHERE proc_id = 49 and fncd_id = 16044;
+DELETE FROM seguranca.funcionalidade WHERE fncd_id = 16044;
 DELETE FROM cobranca.parametro where id = 2;
