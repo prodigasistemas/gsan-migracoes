7a3872ab64a2f1dcf23edadedbdaf2e02a048869
Ewertton Bravo <ewerttonbravo@gmail.com>
2018-09-05 16:50:37 -0300

    Alterando tipo das colunas de coordenada da tabela visita

--- comercial/scripts/20180905192504_add_colunas_na_tabela_visita.sql
+++ comercial/scripts/20180905192504_add_colunas_na_tabela_visita.sql
@@ -1,9 +1,9 @@
 -- // add colunas na tabela visita
 -- Migration SQL that makes the change goes here.
-ALTER TABLE atualizacaocadastral.visita ADD COLUMN vist_nncoordenadax integer;
+ALTER TABLE atualizacaocadastral.visita ADD COLUMN vist_nncoordenadax character varying(50);
 COMMENT ON COLUMN atualizacaocadastral.visita.vist_nncoordenadax IS 'coordenada X do imovel';
 
-ALTER TABLE atualizacaocadastral.visita ADD COLUMN vist_nncoordenaday integer;
+ALTER TABLE atualizacaocadastral.visita ADD COLUMN vist_nncoordenaday character varying(50);
 COMMENT ON COLUMN atualizacaocadastral.visita.vist_nncoordenaday IS 'coordenada Y do imovel';
 
 -- //@UNDO
