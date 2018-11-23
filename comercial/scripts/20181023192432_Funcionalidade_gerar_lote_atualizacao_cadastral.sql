40f800048d501081cac1879952c9fd67d66b174e
Pamela Gatinho <pamelagatinho@gmail.com>
2018-11-23 16:50:40 -0300

    Correcao

--- comercial/scripts/20181023192432_Funcionalidade_gerar_lote_atualizacao_cadastral.sql
+++ comercial/scripts/20181023192432_Funcionalidade_gerar_lote_atualizacao_cadastral.sql
@@ -5,8 +5,8 @@
 ** FUNCIONALIDADE
 **/
 INSERT INTO seguranca.funcionalidade
-VALUES (nextval('seguranca.seq_funcionalidade'), 1, 'Exibir Gerar Lote Atualizacao Cadastral', now(), 'Gsan/Cadastro/Atualizacao Cadastral',
-        'exibirFiltrarGerarLoteAtualizacaoCadastralAction.do', 1, 'ExLtRec', 7,  2, 2, 10, null);
+VALUES (nextval('seguranca.seq_funcionalidade'), 1, 'Gerar Lote Atualizacao Cadastral', now(), 'Gsan/Cadastro/Atualizacao Cadastral',
+        'exibirFiltrarGerarLoteAtualizacaoCadastralAction.do', 1, 'ExLtRec', 121,  2, 2, 10, null);
 
 
 INSERT INTO seguranca.funcionalidade
@@ -15,14 +15,14 @@ VALUES (nextval('seguranca.seq_funcionalidade'), 1, 'Filtrar Imoveis Gerar Lote 
 
 
 INSERT INTO seguranca.funcionalidade
-VALUES (nextval('seguranca.seq_funcionalidade'), 1, 'Gerar Lote Atualizacao Cadastral', now(), 'Gsan/Cadastro/Atualizacao Cadastral',
+VALUES (nextval('seguranca.seq_funcionalidade'), 1, 'Concluir Gerar Lote Atualizacao Cadastral', now(), 'Gsan/Cadastro/Atualizacao Cadastral',
         'gerarLoteAtualizacaoCadastral.do', 2, 'GrLtRec', 0,  2, 2, 10, null);
 
 /**
 ** OPERACAO
 **/
 INSERT INTO seguranca.operacao
-VALUES (nextval('seguranca.seq_operacao'), (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Exibir Gerar Lote Atualizacao Cadastral'), 'Exibir Gerar Lote Atualizacao Cadastral',
+VALUES (nextval('seguranca.seq_operacao'), (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Gerar Lote Atualizacao Cadastral'), 'Gerar Lote Atualizacao Cadastral',
         'LoteRec', null, now(), null, null, 6, null, 2);
 
 INSERT INTO seguranca.operacao
@@ -30,19 +30,19 @@ VALUES (nextval('seguranca.seq_operacao'), (SELECT fncd_id FROM seguranca.funcio
         'FLtRec', null, now(), null, null, 6, null, 2);
 
 INSERT INTO seguranca.operacao
-VALUES (nextval('seguranca.seq_operacao'), (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Gerar Lote Atualizacao Cadastral'), 'Gerar Lote Atualizacao Cadastral',
+VALUES (nextval('seguranca.seq_operacao'), (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Concluir Gerar Lote Atualizacao Cadastral'), 'Concluir Gerar Lote Atualizacao Cadastral',
         'GrLtRec', null, now(), null, null, 6, null, 2);
 
 /**
 ** GRUPO FUNCIONALIDADE OPERACAO
 **/
 INSERT INTO seguranca.grupo_func_operacao
-VALUES (1, (SELECT oper_id FROM seguranca.operacao WHERE oper_dsoperacao LIKE 'Exibir Gerar Lote Atualizacao Cadastral'),
-        (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Exibir Gerar Lote Atualizacao Cadastral'), now());
+VALUES (1, (SELECT oper_id FROM seguranca.operacao WHERE oper_dsoperacao LIKE 'Gerar Lote Atualizacao Cadastral'),
+        (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Gerar Lote Atualizacao Cadastral'), now());
 
 INSERT INTO seguranca.grupo_func_operacao
-VALUES (73, (SELECT oper_id FROM seguranca.operacao WHERE oper_dsoperacao LIKE 'Exibir Gerar Lote Atualizacao Cadastral'),
-        (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Exibir Gerar Lote Atualizacao Cadastral'), now());
+VALUES (73, (SELECT oper_id FROM seguranca.operacao WHERE oper_dsoperacao LIKE 'Gerar Lote Atualizacao Cadastral'),
+        (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Gerar Lote Atualizacao Cadastral'), now());
 
 INSERT INTO seguranca.grupo_func_operacao
 VALUES (1, (SELECT oper_id FROM seguranca.operacao WHERE oper_dsoperacao LIKE 'Filtrar Imoveis Gerar Lote Atualizacao Cadastral'),
@@ -54,24 +54,24 @@ VALUES (73, (SELECT oper_id FROM seguranca.operacao WHERE oper_dsoperacao LIKE '
 
 
 INSERT INTO seguranca.grupo_func_operacao
-VALUES (1, (SELECT oper_id FROM seguranca.operacao WHERE oper_dsoperacao LIKE 'Gerar Lote Atualizacao Cadastral'),
-        (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Gerar Lote Atualizacao Cadastral'), now());
+VALUES (1, (SELECT oper_id FROM seguranca.operacao WHERE oper_dsoperacao LIKE 'Concluir Gerar Lote Atualizacao Cadastral'),
+        (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Concluir Gerar Lote Atualizacao Cadastral'), now());
 
 INSERT INTO seguranca.grupo_func_operacao
-VALUES (73, (SELECT oper_id FROM seguranca.operacao WHERE oper_dsoperacao LIKE 'Gerar Lote Atualizacao Cadastral'),
-        (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Gerar Lote Atualizacao Cadastral'), now());
+VALUES (73, (SELECT oper_id FROM seguranca.operacao WHERE oper_dsoperacao LIKE 'Concluir Gerar Lote Atualizacao Cadastral'),
+        (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Concluir Gerar Lote Atualizacao Cadastral'), now());
 
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-delete from seguranca.usuario_favoritos where fncd_id in (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Exibir Gerar Lote Atualizacao Cadastral');
+delete from seguranca.usuario_favoritos where fncd_id in (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Gerar Lote Atualizacao Cadastral');
 
 DELETE FROM seguranca.grupo_func_operacao
-WHERE fncd_id IN (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Exibir Gerar Lote Atualizacao Cadastral');
+WHERE fncd_id IN (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Gerar Lote Atualizacao Cadastral');
 
-DELETE FROM seguranca.operacao WHERE oper_dsoperacao = 'Exibir Gerar Lote Atualizacao Cadastral';
+DELETE FROM seguranca.operacao WHERE oper_dsoperacao = 'Gerar Lote Atualizacao Cadastral';
 
-DELETE FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade = 'Exibir Gerar Lote Atualizacao Cadastral';
+DELETE FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade = 'Gerar Lote Atualizacao Cadastral';
 
 
 delete from seguranca.usuario_favoritos where fncd_id in (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Filtrar Imoveis Gerar Lote Atualizacao Cadastral');
@@ -84,11 +84,11 @@ DELETE FROM seguranca.operacao WHERE oper_dsoperacao = 'Filtrar Imoveis Gerar Lo
 DELETE FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade = 'Filtrar Imoveis Gerar Lote Atualizacao Cadastral';
 
 
-delete from seguranca.usuario_favoritos where fncd_id in (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Gerar Lote Atualizacao Cadastral');
+delete from seguranca.usuario_favoritos where fncd_id in (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Concluir Gerar Lote Atualizacao Cadastral');
 
 DELETE FROM seguranca.grupo_func_operacao
-WHERE fncd_id IN (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Gerar Lote Atualizacao Cadastral');
+WHERE fncd_id IN (SELECT fncd_id FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade LIKE 'Concluir Gerar Lote Atualizacao Cadastral');
 
-DELETE FROM seguranca.operacao WHERE oper_dsoperacao = 'Gerar Lote Atualizacao Cadastral';
+DELETE FROM seguranca.operacao WHERE oper_dsoperacao = 'Concluir Gerar Lote Atualizacao Cadastral';
 
-DELETE FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade = 'Gerar Lote Atualizacao Cadastral';
+DELETE FROM seguranca.funcionalidade WHERE fncd_dsfuncionalidade = 'Concluir Gerar Lote Atualizacao Cadastral';
