81fda48efa8825db4e5f768f0c3f3d401ae9a887
Ewertton Bravo <ewerttonbravo@gmail.com>
2019-08-16 10:25:49 -0300

    Ajustando para o ID correto na situacao cadastral ATUALIZADO LOJA

--- comercial/scripts/20190813174012_insert_situacao_atualizacao_cadastral_atualizado_loja.sql
+++ comercial/scripts/20190813174012_insert_situacao_atualizacao_cadastral_atualizado_loja.sql
@@ -1,9 +1,9 @@
 -- // insert_situacao_atualizacao_cadastral_atualizado_loja
 -- Migration SQL that makes the change goes here.
-INSERT INTO cadastro.situacao_atlz_cadastral VALUES (12, 'ATUALIZADO LOJA', 1, now());
+INSERT INTO cadastro.situacao_atlz_cadastral VALUES (13, 'ATUALIZADO LOJA', 1, now());
 
 
 -- //@UNDO
 -- SQL to undo the change goes here.
-DELETE FROM cadastro.situacao_atlz_cadastral WHERE siac_id = 12;
+DELETE FROM cadastro.situacao_atlz_cadastral WHERE siac_id = 13;
 
