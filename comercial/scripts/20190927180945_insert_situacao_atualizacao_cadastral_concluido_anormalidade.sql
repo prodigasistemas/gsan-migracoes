0dadc41f303bd93ddd95667d46071053858e6511
Pamela Gatinho <pamelagatinho@gmail.com>
2019-09-30 08:55:28 -0300

    Nova situacao atualizacao cadastral

---
+++ comercial/scripts/20190927180945_insert_situacao_atualizacao_cadastral_concluido_anormalidade.sql
@@ -0,0 +1,11 @@
+-- // insert situacao atualizacao cadastral concluido anormalidade
+-- Migration SQL that makes the change goes here.
+
+INSERT INTO cadastro.situacao_atlz_cadastral VALUES (14, 'CONCLUIDO ANORMALIDA', 1, now());
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+DELETE FROM cadastro.situacao_atlz_cadastral WHERE siac_id = 14;
+
+
