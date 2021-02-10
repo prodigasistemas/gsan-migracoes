e3ddb5d2fcf8979779af4c7a4582ccf6ffd47aab
Felipe Santos <felipesantos2089@gmail.com>
2021-02-10 11:13:07 -0300

    Correções na migração da tabela ordem_servico_foto

--- comercial/scripts/20210203140818_alter_table_ordem_servico_foto.sql
+++ comercial/scripts/20210203140818_alter_table_ordem_servico_foto.sql
@@ -2,16 +2,20 @@
 -- Migration SQL that makes the change goes here.
 
 ALTER TABLE atendimentopublico.ordem_servico_foto NO FORCE ROW LEVEL SECURITY;
-ALTER TABLE atendimentopublico.ordem_servico_foto ALTER COLUMN osft_imfoto DROP NOT NULL;
-ALTER TABLE atendimentopublico.ordem_servico_foto ADD COLUMN osft_nomearquivo character varying(100);
-ALTER TABLE atendimentopublico.ordem_servico_foto ADD COLUMN osft_caminhoarquivo character varying(200);
+ALTER TABLE atendimentopublico.ordem_servico_foto DROP COLUMN osft_imfoto;
+ALTER TABLE atendimentopublico.ordem_servico_foto DROP COLUMN osft_tmultimaalteracao;
+ALTER TABLE atendimentopublico.ordem_servico_foto ADD COLUMN osft_nmfoto character varying(100) not null;
+ALTER TABLE atendimentopublico.ordem_servico_foto ADD COLUMN osft_caminhofoto character varying(200) not null;
+ALTER TABLE atendimentopublico.ordem_servico_foto ADD COLUMN osft_tmultimaalteracao timestamp not null;
     
-COMMENT ON COLUMN atendimentopublico.ordem_servico_foto.osft_nomearquivo IS 'Armazena o nome do arquivo com sua extensao';
-COMMENT ON COLUMN atendimentopublico.ordem_servico_foto.osft_caminhoarquivo IS 'Armazena o caminho absoluto do arquivo';
+COMMENT ON COLUMN atendimentopublico.ordem_servico_foto.osft_nmfoto IS 'Armazena o nome da foto com sua extensao';
+COMMENT ON COLUMN atendimentopublico.ordem_servico_foto.osft_caminhofoto IS 'Armazena o caminho absoluto da foto';
 
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-ALTER TABLE atendimentopublico.ordem_servico_foto ALTER COLUMN osft_imfoto SET NOT NULL;
-ALTER TABLE atendimentopublico.ordem_servico_foto DROP COLUMN osft_nomearquivo;
-ALTER TABLE atendimentopublico.ordem_servico_foto DROP COLUMN osft_caminhoarquivo;
+ALTER TABLE atendimentopublico.ordem_servico_foto DROP COLUMN osft_nmfoto;
+ALTER TABLE atendimentopublico.ordem_servico_foto DROP COLUMN osft_caminhofoto;
+ALTER TABLE atendimentopublico.ordem_servico_foto DROP COLUMN osft_tmultimaalteracao;
+ALTER TABLE atendimentopublico.ordem_servico_foto ADD COLUMN osft_imfoto bytea NOT NULL;
+ALTER TABLE atendimentopublico.ordem_servico_foto ADD COLUMN osft_tmultimaalteracao timestamp not null;
