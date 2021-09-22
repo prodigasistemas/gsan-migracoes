7700ddf35fe24d8c5bb413f03aee6b2989fe620a
guilhermeaguiar <guilherme.aguiar@prodigasistemas.com.br>
2021-09-22 16:53:07 -0300

    Mudança numero dos ids

--- comercial/scripts/20210921145300_insert_imoveis_perfis.sql
+++ comercial/scripts/20210921145300_insert_imoveis_perfis.sql
@@ -2,16 +2,14 @@
 -- Migration SQL that makes the change goes here.
 
 insert into cadastro.imovel_perfil 
-VALUES (NEXTVAL('cadastro.seq_imovel_perfil'), 'BOLSA AGUA R1', 1, now(), 
+VALUES (11, 'BOLSA AGUA R1', 1, now(), 
 2, 1, 1, 2, null, 2, 2, 1, 2);
 
 insert into cadastro.imovel_perfil 
-VALUES (NEXTVAL('cadastro.seq_imovel_perfil'), 'BOLSA AGUA R2', 1, now(), 
+VALUES (12, 'BOLSA AGUA R2', 1, now(), 
 2, 1, 1, 2, null, 2, 2, 1, 2);
 
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-delete from cadastro.imovel_perfil where iper_dsimovelperfil like 'BOLSA AGUA R1';
-
-delete from cadastro.imovel_perfil where iper_dsimovelperfil like 'BOLSA AGUA R2';
+delete from cadastro.imovel_perfil where iper_id in (11, 12);
