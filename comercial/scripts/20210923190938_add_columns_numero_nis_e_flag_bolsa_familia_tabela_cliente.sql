0a81a2affa2c638a20525a71a6affaa706c7e2a9
Felipe Santos <felipesantos2089@gmail.com>
2021-09-23 19:28:20 -0300

    Novas colunas na tabela de cliente - numero nis e ic bolsa familia

--- comercial/scripts/20210923190938_add_columns_numero_nis_e_flag_bolsa_familia_tabela_cliente.sql
+++ comercial/scripts/20210923190938_add_columns_numero_nis_e_flag_bolsa_familia_tabela_cliente.sql
@@ -1,9 +1,11 @@
 -- // add columns numero nis e flag bolsa familia tabela cliente
 -- Migration SQL that makes the change goes here.
 
-
+ALTER TABLE cadastro.cliente ADD COLUMN clie_nnnis INTEGER;
+ALTER TABLE cadastro.cliente ADD COLUMN clie_icbolsafamilia smallint default 2;
 
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-
+ALTER TABLE cadastro.cliente DROP COLUMN clie_nnnis;
+ALTER TABLE cadastro.cliente DROP COLUMN clie_icbolsafamilia;
