fa938b816dbafb3311d9c69c9a427d5f16ac7983
Pamela Gatinho <pamelagatinho@gmail.com>
2021-09-27 14:18:36 -0300

    Script BOLSA AGUA

--- comercial/scripts/20210921145300_insert_imoveis_perfis.sql
+++ comercial/scripts/20210921145300_insert_imoveis_perfis.sql
@@ -1,15 +1,9 @@
 -- // insert_imoveis_perfis.sql
 -- Migration SQL that makes the change goes here.
 
-insert into cadastro.imovel_perfil 
-VALUES (11, 'BOLSA AGUA R1', 1, now(), 
-2, 1, 1, 2, null, 2, 2, 1, 2);
-
-insert into cadastro.imovel_perfil 
-VALUES (12, 'BOLSA AGUA R2', 1, now(), 
-2, 1, 1, 2, null, 2, 2, 1, 2);
+insert into cadastro.imovel_perfil VALUES (11, 'BOLSA AGUA', 1, now(), 2, 1, 1, 2, null, 2, 2, 1, 2);
 
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-delete from cadastro.imovel_perfil where iper_id in (11, 12);
+delete from cadastro.imovel_perfil where iper_id in (11);
