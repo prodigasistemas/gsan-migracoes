52e9c0fcbc9e1662b2c17fafae92602ac4cf93c9
Kurt Matheus <kurtmatheus93@gmail.com>
2022-06-07 13:35:00 -0300

    Inserir na tabela tabela_coluna a coluna imov_icenviocontafisica

---
+++ comercial/scripts/202206071323551_insert_tabela_coluna_imov_icenviocontafisica.sql
@@ -0,0 +1,11 @@
+-- // insert_das credenciais das aplicações que terão acesso a GSAN-API 
+-- Migration SQL that makes the change goes here.
+
+INSERT INTO seguranca.tabela_coluna(
+	tbco_id, tabe_id, tbco_tmultimaalteracao, tbco_nmcoluna, tbco_dscoluna, tbco_icprimarykey, tbco_nmabreviado, atrb_id, tbco_icpoderetificarconta, tbco_icatucadastral)
+	VALUES (nextval('seq_tabela_coluna'), 52, now(), 'imov_icenviocontafisica', 'imov_icenviocontafisica', 2, null, null, 2, 2);
+	
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+	DELETE FROM seguranca.tabela_coluna WHERE tbco_nmcoluna = 'imov_icenviocontafisica';
