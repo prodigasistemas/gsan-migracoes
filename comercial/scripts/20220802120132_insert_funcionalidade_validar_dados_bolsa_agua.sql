33a1e23325954066dd982b8d20d766142da6da36
Kurt Matheus <kurtmatheus93@gmail.com>
2022-08-24 15:03:09 -0300

    Criar tabela parametros convenio

--- comercial/scripts/20220802120132_insert_funcionalidade_validar_dados_bolsa_agua.sql
+++ comercial/scripts/20220802120132_insert_funcionalidade_validar_dados_bolsa_agua.sql
@@ -1,9 +1,9 @@
 -- // insert_funcionalidade_batch_registrar_boletos.sql
 -- Migration SQL that makes the change goes here.
 
-INSERT INTO seguranca.funcionalidade
-(fncd_id, modu_id, fncd_dsfuncionalidade, fncd_tmultimaalteracao, fncd_dscaminhomenu, fncd_dscaminhourl, fncd_icpontoentrada, fncd_dsabreviado, fncd_nnordemmenu, fncd_icnovajanela, fncd_icolap, fncg_id, url)
-VALUES(nextval('seguranca.seq_funcionalidade'), 8, 'Atualizar Dados Agua Para', now(), null, null, 2, 'AtuAguaPa', 0, 2, 2, null, null);
+--INSERT INTO seguranca.funcionalidade
+--(fncd_id, modu_id, fncd_dsfuncionalidade, fncd_tmultimaalteracao, fncd_dscaminhomenu, fncd_dscaminhourl, fncd_icpontoentrada, fncd_dsabreviado, fncd_nnordemmenu, fncd_icnovajanela, fncd_icolap, fncg_id, url)
+--VALUES(nextval('seguranca.seq_funcionalidade'), 8, 'Atualizar Dados Agua Para', now(), null, null, 2, 'AtuAguaPa', 0, 2, 2, null, null);
 
 -- //@UNDO
 -- SQL to undo the change goes here.
---
+++ comercial/scripts/20220824130842_create_parametros_convenio.sql
@@ -0,0 +1,27 @@
+-- // create parametros convenio
+-- Migration SQL that makes the change goes here.
+
+CREATE SEQUENCE arrecadacao.seq_parametros_convenio
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1; 
+ALTER TABLE arrecadacao.seq_parametros_convenio OWNER TO gsan_admin;
+CREATE TABLE arrecadacao.parametros_convenio
+(
+  prcv_id integer NOT NULL DEFAULT nextval('arrecadacao.seq_parametros_convenio'),
+  prcv_nnconvenio integer NOT NULL,
+  prcv_nncarteira integer NOT NULL,
+  prcv_nnvariacaocarteira integer NOT NULL, 
+  prcv_cdtipotitulo integer NOT NULL,
+  CONSTRAINT parametros_convenio_pkey PRIMARY KEY (prcv_id)
+  );
+    
+ALTER TABLE arrecadacao.parametros_convenio OWNER TO gsan_admin;
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+DROP TABLE arrecadacao.parametros_convenio;
+DROP SEQUENCE arrecadacao.seq_parametros_convenio;
