d5e2040294c35652ac50c080dcfbf67eb28ed597
Kurt Matheus <kurtmatheus93@gmail.com>
2022-08-24 15:13:49 -0300

    criar tabela parametros convenio

--- comercial/scripts/20220824175213_insert_tabela_coluna_imov_icenviocontafisica.sql
+++ comercial/scripts/20220824175213_insert_tabela_coluna_imov_icenviocontafisica.sql
@@ -1,9 +1,9 @@
 -- // insert tabela coluna imov_icenviocontafisica
 -- Migration SQL that makes the change goes here.
 
-INSERT INTO seguranca.tabela_coluna(
-tbco_id, tabe_id, tbco_tmultimaalteracao, tbco_nmcoluna, tbco_dscoluna, tbco_icprimarykey, tbco_nmabreviado, atrb_id, tbco_icpoderetificarconta, tbco_icatucadastral)
-VALUES (nextval('seguranca.seq_tabela_coluna'), 52, now(), 'imov_icenviocontafisica', 'imov_icenviocontafisica', 2, null, null, 2, 2);
+--INSERT INTO seguranca.tabela_coluna(
+--tbco_id, tabe_id, tbco_tmultimaalteracao, tbco_nmcoluna, tbco_dscoluna, tbco_icprimarykey, tbco_nmabreviado, atrb_id, tbco_icpoderetificarconta, tbco_icatucadastral)
+--VALUES (nextval('seguranca.seq_tabela_coluna'), 52, now(), 'imov_icenviocontafisica', 'imov_icenviocontafisica', 2, null, null, 2, 2);
 	
 -- //@UNDO
 -- SQL to undo the change goes here.
