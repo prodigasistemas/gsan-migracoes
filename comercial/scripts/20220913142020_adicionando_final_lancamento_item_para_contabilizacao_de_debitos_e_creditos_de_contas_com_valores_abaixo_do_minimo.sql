751211fa692ff75c74decd6b327c796fa30916ee
Felipe Santos <felipe.santos@prodigasistemas.com.br>
2022-09-14 10:49:51 -0300

    inclusao do lancamento item

---
+++ comercial/scripts/20220913142020_adicionando_final_lancamento_item_para_contabilizacao_de_debitos_e_creditos_de_contas_com_valores_abaixo_do_minimo.sql
@@ -0,0 +1,19 @@
+-- // adicionando final lancamento_item para contabilizacao de debitos e creditos de contas com valores abaixo do minimo
+-- Migration SQL that makes the change goes here.
+
+insert into financeiro.lancamento_item values
+	(135, 'FATURAMENTO INF MINIMO', 'FATIM', 2, now());
+
+insert into financeiro.lancamento_item values
+	(136, 'CREDITO BAIXA FATURAMENTO INF MINIMO', 'CREDBFIM', 2, now()); 
+
+
+	
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+delete from financeiro.lancamento_item where lcit_id = 135;
+
+delete from financeiro.lancamento_item where lcit_id = 136;
+
+
