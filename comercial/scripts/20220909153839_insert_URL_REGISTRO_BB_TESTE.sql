67e2940fcaa7b4f45d8313b61f7a5474a4866f4c
kurtmatheus <kurtmatheus93@gmail.com>
2022-10-11 11:19:31 -0300

    comentando migracoes ja feitas

--- comercial/scripts/20220909153839_insert_URL_REGISTRO_BB_TESTE.sql
+++ comercial/scripts/20220909153839_insert_URL_REGISTRO_BB_TESTE.sql
@@ -1,7 +1,7 @@
 -- // insert URL REGISTRO BB TESTE
 -- Migration SQL that makes the change goes here.
 
-INSERT INTO seguranca.parametro values (13, 'URL_REGISTRO_BB_TESTE', 'http://api.cosanpa.pa.gov.br/registro_boleto_teste/');
+--INSERT INTO seguranca.parametro values (13, 'URL_REGISTRO_BB_TESTE', 'http://api.cosanpa.pa.gov.br/registro_boleto_teste/');
 
 -- //@UNDO
 -- SQL to undo the change goes here.
--- comercial/scripts/20220913142020_adicionando_final_lancamento_item_para_contabilizacao_de_debitos_e_creditos_de_contas_com_valores_abaixo_do_minimo.sql
+++ comercial/scripts/20220913142020_adicionando_final_lancamento_item_para_contabilizacao_de_debitos_e_creditos_de_contas_com_valores_abaixo_do_minimo.sql
@@ -1,11 +1,11 @@
 -- // adicionando final lancamento_item para contabilizacao de debitos e creditos de contas com valores abaixo do minimo
 -- Migration SQL that makes the change goes here.
 
-insert into financeiro.lancamento_item values
-	(135, 'FATURAMENTO INF MINIMO', 'FATIM', 2, now());
-
-insert into financeiro.lancamento_item values
-	(136, 'CREDITO BAIXA FATURAMENTO INF MINIMO', 'CREDBFIM', 2, now()); 
+--insert into financeiro.lancamento_item values
+--	(135, 'FATURAMENTO INF MINIMO', 'FATIM', 2, now());
+--
+--insert into financeiro.lancamento_item values
+--	(136, 'CREDITO BAIXA FATURAMENTO INF MINIMO', 'CREDBFIM', 2, now()); 
 
 
 	
