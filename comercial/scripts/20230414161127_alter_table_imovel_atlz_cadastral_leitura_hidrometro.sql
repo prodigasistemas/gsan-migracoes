a8177e37994741c59b457d3f21f71e87dd8915b9
kurtmatheus <kurtmatheus93@gmail.com>
2023-04-17 10:58:39 -0300

    config[dados-leitura-hidrometro]

---
+++ comercial/scripts/20230414161127_alter_table_imovel_atlz_cadastral_leitura_hidrometro.sql
@@ -0,0 +1,12 @@
+-- // alter table imovel atlz cadastral leitura hidrometro
+-- Migration SQL that makes the change goes here.
+
+ALTER TABLE cadastro.imovel_atlz_cadastral
+ADD COLUMN "imac_nnleiturahidrometro" VARCHAR(10); 
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+ALTER TABLE cadastro.imovel_atlz_cadastral DROP COLUMN imac_nnleiturahidrometro; 
+
+
---
+++ comercial/scripts/20230414161812_alter_table_imovel_retorno_leitura_hidrometro.sql
@@ -0,0 +1,10 @@
+-- // alter table imovel retorno leitura hidrometro
+-- Migration SQL that makes the change goes here.
+
+ALTER TABLE atualizacaocadastral.imovel_retorno
+ADD COLUMN "imre_nnleiturahidrometro" VARCHAR(10); 
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+ALTER TABLE atualizacaocadastral.imovel_retorno DROP COLUMN imre_nnleiturahidrometro; 
