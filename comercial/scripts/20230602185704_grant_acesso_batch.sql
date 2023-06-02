f59ffccfaf1430436d0adde600c30390d55976c0
guilhermeaguiar <guilherme.aguiar@prodigasistemas.com.br>
2023-06-02 15:58:41 -0300

    acesso gsan_batch

---
+++ comercial/scripts/20230602185704_grant_acesso_batch.sql
@@ -0,0 +1,9 @@
+-- // grant_acesso_batch
+-- Migration SQL that makes the change goes here.
+
+GRANT ALL ON TABLE cadastro.cadastro_agua_para TO gsan_batch;
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+
