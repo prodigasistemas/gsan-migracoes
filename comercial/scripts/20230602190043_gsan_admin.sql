8b45fe405897e477b14ce2b180502c61ed0631e1
guilhermeaguiar <guilherme.aguiar@prodigasistemas.com.br>
2023-06-02 16:02:07 -0300

    grant gsan admin

---
+++ comercial/scripts/20230602190043_gsan_admin.sql
@@ -0,0 +1,10 @@
+-- // gsan_admin
+-- Migration SQL that makes the change goes here.
+
+GRANT ALL ON TABLE cadastro.cadastro_agua_para TO gsan_admin;
+GRANT ALL ON TABLE cadastro.seq_cadastro_agua_para TO gsan_admin;
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+
