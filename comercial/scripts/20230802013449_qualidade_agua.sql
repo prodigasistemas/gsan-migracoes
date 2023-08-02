611da689c09c24d81784591499730a0a4e9e0038
guilherme <guilherme.aguiar@prodigasistemas.com.br>
2023-08-01 22:44:26 -0300

    qualidade agua

--- comercial/scripts/20230802013449_qualidade_agua.sql
+++ comercial/scripts/20230802013449_qualidade_agua.sql
@@ -1,8 +1,7 @@
--- // informações_sobre_controle_da_qualidade_da_agua
+-- // qualidade_agua
 -- Migration SQL that makes the change goes here.
 
 ALTER TABLE faturamento.qualidade_agua_padrao ADD COLUMN qlap_dspadraoecoli varchar(20);
-
 ALTER TABLE faturamento.qualidade_agua ADD COLUMN qlag_nnindiceecoli NUMERIC(5,2);
 ALTER TABLE faturamento.qualidade_agua ADD COLUMN qlag_qtecoliexigidas int4;
 ALTER TABLE faturamento.qualidade_agua ADD COLUMN qlag_qtecolianalisadas int4;
@@ -11,4 +10,8 @@ ALTER TABLE faturamento.qualidade_agua ADD COLUMN qlag_qtecoliconforme int4;
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-
+ALTER TABLE faturamento.qualidade_agua_padrao DROP COLUMN qlap_dspadraoecoli;
+ALTER TABLE faturamento.qualidade_agua DROP COLUMN qlag_nnindiceecoli;
+ALTER TABLE faturamento.qualidade_agua DROP COLUMN qlag_qtecoliexigidas;
+ALTER TABLE faturamento.qualidade_agua DROP COLUMN qlag_qtecolianalisadas;
+ALTER TABLE faturamento.qualidade_agua DROP COLUMN qlag_qtecoliconforme;
